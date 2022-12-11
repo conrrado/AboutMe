a40089fb7328189d2752883f37a6a7851d3527e6
Conrrado Camacho <con.webmaster@gmail.com>
2022-12-11 12:09:23 -0300

    Title: Use Data Class to define the data in the view
    Body: Added data class, code refactoring

--- app/src/main/java/com/conrradocamacho/aboutme/MainActivity.kt
+++ app/src/main/java/com/conrradocamacho/aboutme/MainActivity.kt
@@ -11,18 +11,22 @@ import com.conrradocamacho.aboutme.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.myName = myName
         binding.doneButton.setOnClickListener { addNickname(it) }
         binding.nicknameText.setOnClickListener { updateNickname(it) }
     }
 
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = nicknameEdit.text.toString()
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
